d735b748ba44905f66bfe56407090af4884760e9
holy0111 <sof.hirano@gmail.com>
2026-06-07 20:47:54 +0900

    Update diagnose.js

--- pages/api/diagnose.js
+++ pages/api/diagnose.js
@@ -14,12 +14,15 @@ export default async function handler(req, res) {
 
   const requestData = {
     inputs: {
-      artist1: `${artist1}（好きな点：${point1}）`,
+      artist1: artist1,
       title1: title1,
-      artist2: `${artist2}（好きな点：${point2}）`,
+      point1: point1,
+      artist2: artist2,
       title2: title2,
-      artist3: `${artist3}（好きな点：${point3}）`,
+      point2: point2,
+      artist3: artist3,
       title3: title3,
+      point3: point3,
       query: "各楽曲の横にある『好きな点』の指定（メロディか歌詞か）を絶対的な基準として、指定されたフォーマットで診断を実行してください。"
     },
     query: "診断開始",
